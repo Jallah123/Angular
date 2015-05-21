17953deae4d0d73c9d9231f908b2cb5b1dc42ca8
tojba <yougotspam123@gmail.com>
2015-05-21 11:35:00 +0200

    remove routeparameters by redirect

--- app/js/controllers/AuthController.js
+++ app/js/controllers/AuthController.js
@@ -1,5 +1,5 @@
 module.exports = function($location, $routeParams, UserFactory) {
 	UserFactory.saveUser($routeParams['username'], $routeParams['token']);
 	console.log($routeParams);
-	$location.path('/');
+	$location.url($location.path('/'));
 };
