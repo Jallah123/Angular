e0f041a7fb63e2d161e6e80cbc3eb3b9ea5ef7ef
Tojba <yougotspam1232gmail.com>
2015-06-20 13:28:45 +0200

    added filter tests

---
+++ test/game-filter.spec.js
@@ -0,0 +1,46 @@
+describe("Game Filters", function() {
+	var gameFilter;
+
+	/*
+		We vragen hier de module op die we in de app.js gecreëerd hebben.
+		Angular-mocks regelt voor ons dat alles geïnitialiseerd wordt.
+	*/
+	beforeEach(module('angular'));
+
+	/*
+		We kunnen verchillende dingen van de app opvragen.
+		Services, filters, directives, controllers, scopes, etc
+
+		Angular-mocks gebruikt de inject functie hiervoor.
+
+		We kunnen underscores voor en achter de providers zetten, angular weet dan nog steeds welke providers het zijn
+	*/
+	beforeEach(inject(function($filter){
+		gameFilter = $filter('gameState');
+	}));
+
+	it('should return open', function(done){
+		// Filters zijn het gemakkelijkst te testen omdat ze jouw zelf gemaakte functie returnen.
+		var result = gameFilter({ state: "open" }, "open");
+		expect(result).to.equal(true);
+
+		done();
+	});
+
+	it('should not equal finished to open', function(done){
+		// Filters zijn het gemakkelijkst te testen omdat ze jouw zelf gemaakte functie returnen.
+		var result = gameFilter({ state: "finished" }, "open");
+		expect(result).to.equal(false);
+
+		done();
+	});
+
+	it('should equal finished to finished', function(done){
+		// Filters zijn het gemakkelijkst te testen omdat ze jouw zelf gemaakte functie returnen.
+		var result = gameFilter({ state: "finished" }, "finished");
+		expect(result).to.equal(true);
+
+		done();
+	});
+
+});
